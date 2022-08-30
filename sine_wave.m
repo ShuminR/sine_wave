43b95c3598fa0b64a973447e3d5206f296cf89a1
ShuminR <97569849+ShuminR@users.noreply.github.com>
2022-08-30 13:16:19 -0400

    Update sine_wave.m

--- sine_wave.m
+++ sine_wave.m
@@ -1,35 +1,35 @@
-clc; %清除命令行命令
-clear all; %清除工作区变量,释放内存空间
-F1=1; %信号频率
-Fs=2^8; %采样频率
-P1=0; %信号初始相位
-N=2^8; %采样点数
-t=[0:1/Fs:(N-1)/Fs]; %采样时刻
-ADC=2^7 - 1; %直流分量
-A=2^7; %信号幅度
-%生成正弦信号
+clc; %clear command
+clear all; %Clears the workspace variable, freeing memory
+F1=1; %signal frequency
+Fs=2^8; %sampling frequency
+P1=0; %Initial signal phase
+N=2^8; %sampling points
+t=[0:1/Fs:(N-1)/Fs]; %sampling time
+ADC=2^7 - 1; %DC component
+A=2^7; %signal amplitude
+% generates sinusoidal signal
 s=A*sin(2*pi*F1*t + pi*P1/180) + ADC;
-plot(s); %绘制图形
-%创建 mif 文件
+plot(s); %Draw a graph
+% Creates an MIF file
 fild = fopen('sin_wave_4096x8.mif','wt');
-%写入 mif 文件头
-fprintf(fild, '%s\n','WIDTH=8;'); %位宽
-fprintf(fild, '%s\n\n','DEPTH=4096;'); %深度
-fprintf(fild, '%s\n','ADDRESS_RADIX=UNS;'); %地址格式
-fprintf(fild, '%s\n\n','DATA_RADIX=UNS;'); %数据格式
-fprintf(fild, '%s\t','CONTENT'); %地址
-fprintf(fild, '%s\n','BEGIN'); %开始
+% is written to the MIF header
+fprintf(fild, '%s\n','WIDTH=8;'); %width
+fprintf(fild, '%s\n\n','DEPTH=4096;'); %depth
+fprintf(fild, '%s\n','ADDRESS_RADIX=UNS;'); % address format
+fprintf(fild, '%s\n\n','DATA_RADIX=UNS;'); % data format
+fprintf(fild, '%s\t','CONTENT'); %address
+fprintf(fild, '%s\n','BEGIN'); %begin
 for i = 1:N
-s0(i) = round(s(i)); %对小数四舍五入以取整
-if s0(i) <0 %负 1 强制置零
+s0(i) = round(s(i)); 
+if s0(i) <0 %minus 1 Force zero
 s0(i) = 0
 end
 fprintf(fild, 'sin[',i-1); 
-fprintf(fild, '\t%g\t',i-1); %地址编码
+fprintf(fild, '\t%g\t',i-1); %Address code
 fprintf(fild, ']',i-1); 
-fprintf(fild, '%s\t',':'); %冒号
-fprintf(fild, '%d',s0(i)); %数据写入
-fprintf(fild, '%s\n',';'); %分号，换行
+fprintf(fild, '%s\t',':'); %colon
+fprintf(fild, '%d',s0(i)); %data input
+fprintf(fild, '%s\n',';'); %semicolon
 end
-fprintf(fild, '%s\n','END;'); %结束
+fprintf(fild, '%s\n','END;'); %end
 fclose(fild);
